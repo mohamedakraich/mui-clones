e63bbe6b685310586579b95a7a3ea37a9ced3d82
goenix <mohamed.akraich@gmail.com>
2023-02-07 10:50:37 +0100

    scaffolding for PopularPosts added

---
+++ components/reddit/main-content/PopularPosts.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+import {
+    postSectionWrapper,
+    communitiesWrapper,
+    postSortWrapper,
+    popularPostHeader,
+    postSortContainer,
+} from '@/styles/reddit/styles';
+import { cardContentItems } from '@/data/reddit/app.data';
+
+const PopularPosts = () => {
+    return (
+        <React.Fragment>
+            <Typography sx={popularPostHeader} variant="h6">
+                Popular Posts
+            </Typography>
+            <Box sx={postSortContainer}>
+                <Box sx={postSectionWrapper}>
+                    <Box sx={postSortWrapper}>
+                        {/* <PostSort /> */}
+                    </Box>
+                    {cardContentItems.map((item) => {
+                        return <p key={item.id}>InfoCard</p>;
+                    })}
+                </Box>
+                <Box sx={communitiesWrapper}>
+                    {/* <PopularCommunities /> */}
+                    {/* <Policy /> */}
+                </Box>
+            </Box>
+        </React.Fragment>
+    );
+};
+
+export default PopularPosts;
