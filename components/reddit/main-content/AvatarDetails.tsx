e6ce8d438aea0988a5b2ee478054abb51b25de70
goenix <mohamed.akraich@gmail.com>
2023-02-07 10:10:41 +0100

    component AvatarDetails added

---
+++ components/reddit/main-content/AvatarDetails.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Box, Typography } from '@mui/material';
+
+import { avatarWrapper, flex } from '@/styles/reddit/styles';
+
+interface AvatarDetailsProps {
+    icon: JSX.Element;
+    user: string;
+}
+
+const AvatarDetails = ({ icon, user }: AvatarDetailsProps) => {
+    return (
+        <React.Fragment>
+            <Box sx={avatarWrapper}>
+                <Box sx={{ mr: 1, ...flex }}>
+                    {icon}
+                </Box>
+                <Typography variant="caption"> {user}</Typography>
+            </Box>
+        </React.Fragment>
+    );
+};
+
+export default AvatarDetails;
+
+{/*   <Box as="span" sx={{ mr: 1, ...flex }}> */ }
