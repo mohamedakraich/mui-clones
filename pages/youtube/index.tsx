4fd3a7bb17610606e1c0bd2fd09c88f06727be25
goenix <mohamed.akraich@gmail.com>
2023-02-27 20:17:21 +0100

    fix bug in layout

--- pages/youtube/index.tsx
+++ pages/youtube/index.tsx
@@ -4,11 +4,13 @@ import Box from "@mui/material/Box";
 import { appContentWrapper } from "@/styles/youtube/styles";
 import AppNavMenu from "@/components/youtube/navigation/AppNavMenu";
 import AppThemeProvider from "@/themes/youtube/AppThemeProvider";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, useTheme } from "@mui/material";
 import AppContentArea from "@/components/youtube/AppContentArea";
 
 function YoutubePage() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const theme = useTheme();
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
@@ -18,7 +20,14 @@ function YoutubePage() {
     <React.StrictMode>
       <CssBaseline />
       <AppThemeProvider>
-        <Box sx={appContentWrapper}>
+        <Box
+          sx={{
+            display: "flex",
+            mt: theme.mixins.toolbar.height,
+            width: "100vw",
+            overflow: "hidden",
+          }}
+        >
           <AppNavMenu handleDrawerToggle={handleDrawerToggle} />
           <AppContentArea isOpen={mobileOpen} />
         </Box>
