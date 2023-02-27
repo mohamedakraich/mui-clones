82a9d6e245bdc1e93bdec85b739af1fd46929daa
goenix <mohamed.akraich@gmail.com>
2023-02-27 16:29:41 +0100

    add SideList youtube comp

---
+++ components/youtube/SideList.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import { sideListWrapper } from "@/styles/youtube/styles";
+import { sideListItems } from "@/data/youtube/app.data";
+
+const SideList = () => {
+  return (
+    <Box sx={sideListWrapper}>
+      {sideListItems.map((item) => {
+        return (
+          <React.Fragment key={item.id}>
+            {item.subdivision ? (
+              <>
+                <Divider />
+                <Box sx={{ ml: 2, mt: 2 }}>
+                  <Typography
+                    sx={{ textTransform: "uppercase", fontWeight: "bold" }}
+                    variant="button"
+                    display="block"
+                    gutterBottom
+                  >
+                    {item.text}
+                  </Typography>
+                </Box>
+              </>
+            ) : item.divider ? (
+              <Divider />
+            ) : (
+              <nav aria-label="Side list items">
+                <List sx={{ p: 0 }}>
+                  <ListItem disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>{item.icon}</ListItemIcon>
+                      <ListItemText primary={item.text} />
+                    </ListItemButton>
+                  </ListItem>
+                </List>
+              </nav>
+            )}
+          </React.Fragment>
+        );
+      })}
+    </Box>
+  );
+};
+
+export default SideList;
